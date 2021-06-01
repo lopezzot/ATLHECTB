8d08d60212dbdf7890f566dcfe5aa16bbc1b38f3
Lorenzo Pezzotti <lorenzo.pezzotti01@gmail.com>
2021-06-01 17:25:20 +0200

    test with random seed

--- ATLHECTB.cc
+++ ATLHECTB.cc
@@ -77,6 +77,7 @@ int main( int argc, char** argv ) {
     #ifdef G4MULTITHREADED
     if ( nthreads > 0 ) runManager->SetNumberOfThreads(nthreads);
     #endif
+    G4Random::setTheSeed(2);
 
     //Set mandatory classes (DetConstruction, PhysicsList, ActionInitialization)
     //
