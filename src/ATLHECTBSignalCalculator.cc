4619b75eaa28fdcd76cd871bffd4b016fa79ce00
Lorenzo Pezzotti <lorenzo.pezzotti01@gmail.com>
2021-06-07 16:46:01 +0200

    Birk as in Athena

--- src/ATLHECTBSignalCalculator.cc
+++ src/ATLHECTBSignalCalculator.cc
@@ -33,12 +33,12 @@ ATLHECTBSignalCalculator* ATLHECTBSignalCalculator::Instance(){
 G4double ATLHECTBSignalCalculator::ApplyBirks( const G4double& de, const G4double& steplength ){
 
             const G4double Emin = 1.51;        //(MeV cm2)/g, minimal energy loss in LAr
-            const G4double Ef = 9.2;           //kV/cm, electric field in LAr gaps  
+            const G4double Ef = 10.0;           //kV/cm, electric field in LAr gaps  
             const G4double k = 0.0486;         //kV/cm g/(MeV cm2), paramter in Birks equation
             const G4double A = 1.+(k/Ef)*Emin; //Parameter in Birks equation
             const G4double LArdensity = 1.396; //g/cm3
 
-            G4double satde = de*A/( 1.+( (k/Ef)*(1/LArdensity)*(de/steplength) ) ); 
+            G4double satde = de*A/( 1.+ (k/Ef)*(1/LArdensity)*(de/steplength) ); 
     
             return satde;
 }
